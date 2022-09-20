90cbd3262dfecaa27526b3b26334b67a744b6ea2
LENOVO <frisli.089.df@gmail.com>
2022-09-20 21:42:33 +0300

    8.2 выполненное задание

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         var generalWorkHours = 640;
-        System.out.println(generalWorkHours);
+        System.out.println("Всего отработано " + generalWorkHours + " часов");
         var oneWorkerHours = 8;
-        System.out.println(oneWorkerHours);
+        System.out.println("Один сотрудник отрабатывает " + oneWorkerHours + " часов");
         var generalWorkersAmount = generalWorkHours / oneWorkerHours;
         System.out.println("Всего работников в компании " + generalWorkersAmount + " человек");
         var workersAmount = generalWorkersAmount + 94;
-        System.out.println(workersAmount);
+        System.out.println("После добавления людей количество сотрудников составило " + workersAmount + " человек");
         var hoursAmount = workersAmount * oneWorkerHours;
         System.out.println("Если в компании работает " + workersAmount + " человек, то всего " + hoursAmount + " часов работы может быть поделено между сотрудниками.");
         
